53a2fb3a8d968147d49e8c151d4e81146919000d
soumyabhatg <40351176+soumyabhatg@users.noreply.github.com>
2018-07-04 12:07:34 +0530

    Update demo.java

--- demo.java
+++ demo.java
@@ -32,6 +32,7 @@ System.setProperty("webdriver.chrome.driver","/home/avohi/Downloads/chromedriver
 		
 		}
 	System.out.println("NUMBER OF BUTTONS="+buttons.size());
+		System.out.println("************************");
 		
 	}
 }
